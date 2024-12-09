7b21fe659c826a1cdde9dcab01264ed52ba0df95
Fasih Muhammad Virk <fasih.muhammad@icloud.com>
2024-12-09 15:31:11 +0500

    commenting and improvements in return statements

--- src/utils.py
+++ src/utils.py
@@ -96,8 +96,9 @@ def get_year_month(date : str) -> tuple:
 
     return year, month
 
-def get_switch_and_date(list_of_switches_and_dates : list):
+def get_switch_and_date(list_of_switches_and_dates : list) -> tuple:
 
+    # generating the value of each switch and date from the list
     index_to_find_date = 1
     for index_to_find_switch in range(0 , len(list_of_switches_and_dates) , 2):
 
@@ -108,46 +109,105 @@ def get_switch_and_date(list_of_switches_and_dates : list):
 
         yield switch , date
 
-def validate_switches_and_date(list_of_switches_and_dates : list ):
+def validate_date(year : str , month : str) -> bool:
 
+    if (int(year) <= 2006) and (int(year) >= 2004):
+
+        if month is None:
+            return True
+
+        else:
+
+            if (int(month) > 0) and (int(month) < 12):
+                return True
+
+            else:
+                print("Please Enter a Valid Month")
+                return False
+
+    else:
+        print("This is Not a Valid Year, we donot have data on this year")
+        return False
+
+
+def validate_switch(switch : str , month : str) -> bool:
+
+    # list of the valid switches
+    list_of_valid_switches = ['-e', '-a', '-c']
+
+    # checking if the switch enterd is correct
+    if switch in list_of_valid_switches:
+
+        if switch == list_of_valid_switches[0] and month is not None:
+            print(print(f"{switch} This switch date should not contain value of month"))
+            return False
+
+        elif switch == list_of_valid_switches[1] and month is None:
+            print(print(f"{switch} This switch date should contain value of month"))
+            return False
+
+        elif switch == list_of_valid_switches[2] and month is None:
+            print(print(f"{switch} This switch date should contain value of month"))
+            return False
+
+        else:
+            return True
+
+    else:
+        print(f"{switch} This is not a valid switch")
+        return False
+
+
+
+def validate_switches_and_date(list_of_switches_and_dates : list ) -> bool:
+
+    # checking if the user has enter more than three values of switch and date
     if len(list_of_switches_and_dates)//2 > 3:
         print("Please Enter only three values of switch and date to get Report")
-        return []
+        return False
 
+    # checking if all the switches have their values
     if len(list_of_switches_and_dates) % 2 != 0:
         print("One of the Switch or Date is Missing")
-        return []
+        return False
 
-    list_of_valid_switches = ['-e', '-a', '-c']
     index_to_find_date = 1
+    valid_input = False
+
     for index_to_find_switch in range(0, len(list_of_switches_and_dates), 2):
 
+        # getting value of each switch and its date
         switch = list_of_switches_and_dates[index_to_find_switch]
         date = list_of_switches_and_dates[index_to_find_date]
-        year , month = get_year_month(date)
 
-        if switch in list_of_valid_switches:
-
-            if switch == list_of_valid_switches[0] and month is not None:
-                print(print(f"{switch} This switch date should not contain value of month"))
-                return []
-
-            elif switch == list_of_valid_switches[1] and month is None:
-                print(print(f"{switch} This switch date should contain value of month"))
-                return []
-
-            elif switch == list_of_valid_switches[2] and month is None:
-                print(print(f"{switch} This switch date should contain value of month"))
-                return []
-        else:
-            print(f"{switch} This is not a valid switch")
-            return []
-
+        # index to find date in the list
         index_to_find_date += 2
 
-    return True
+        # converting the date into year and month
+        year , month = get_year_month(date)
 
-def validate_input(command_line_arguments):
+        #validating if the format is correct also the date
+        is_year_month_valid = validate_date(year , month)
+
+
+        if is_year_month_valid:
+
+            # validating the switches
+            is_switch_valid = validate_switch(switch , month)
+
+            if is_switch_valid:
+                valid_input = True
+
+            else:
+                valid_input = False
+
+        else:
+            valid_input = False
+
+    return valid_input
+
+def validate_input(command_line_arguments : list) -> bool:
+
     valid_path = command_line_arguments[0]
 
     if os.path.isdir(valid_path):
@@ -156,6 +216,7 @@ def validate_input(command_line_arguments):
 
         if is_switch_date_valid:
             return True
+
         else:
             return False
     else:
@@ -164,7 +225,8 @@ def validate_input(command_line_arguments):
         return False
 
 
-def process_input(command_line_arguments):
+def process_input(command_line_arguments : list) -> tuple:
+
     path = command_line_arguments[0]
     list_of_switches_and_date = command_line_arguments[1:]
 
